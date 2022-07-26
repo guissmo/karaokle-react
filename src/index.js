29dac07b7bab706d1b1ec5bf5dc29318dfa7511c
Guissmo Asuncion <guissmo@gmail.com>
2022-07-26 10:07:19 +0200

    added production script

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Game from "./Game";
 
 const App = () => {
+  useEffect(() => {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+      console.log("DEVELOPMENT");
+    } else {
+      console.log("PRODUCTION");
+    }
+  });
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/test" element={"yo!!!"} />
         <Route path="/en" element={<Game language="EN" />} />
-        {/* <Route path="/tl" element={<Game language="TL" />} />
+        <Route path="/tl" element={<Game language="TL" />} />
         <Route path="/en/:id" element={<Game language="EN" />} />
-        <Route path="/tl/:id" element={<Game language="TL" />} /> */}
+        <Route path="/tl/:id" element={<Game language="TL" />} />
+        <Route path="/test" element={"yo!!!"} />
+        <Route path="/" element={"under construction"} />
       </Routes>
     </BrowserRouter>
   );
